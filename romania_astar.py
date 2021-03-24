36190265af6471a64b5fdc49b5b5ae7505cc5e57
Guilherme Oliveira do Valle <guidovalle1608@gmail.com>
2021-03-24 08:17:52 -0300

    Inicia implementação dos estados Romania astar

--- romania_astar.py
+++ romania_astar.py
@@ -1,2 +1,13 @@
+mapa_romania = {
+    'Arad' : { 'conexoes': [ { 'cidade':'Zerind', 'distancia': 75},
+                             { 'cidade':'Timisoara', 'distancia': 118},
+                             { 'cidade':'Sibiu', 'distancia': 140} ] },
+
+    'Sibiu' : { 'conexoes': [ { 'cidade':'Fagaras', 'distancia': 99},
+                              { 'cidade':'Rimnicu Vilcea', 'distancia': 80},
+                              { 'cidade':'Oradea', 'distancia': 151},
+                              { 'cidade':'Arad', 'distancia': 140} ]  },
+}
+
 def romania_astar():
-    print('romania')
+    print(mapa_romania['Arad']['conexoes'][0]['cidade'])
