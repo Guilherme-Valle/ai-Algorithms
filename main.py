456dc5702e0c7011aed88824f8795fcc472452a4
Guilherme-Valle <guidovalle1608@gmail.com>
2021-04-13 21:04:18 -0300

    Adiciona estados mapa de paris

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from budget_dfs import solucao_balde_dfs
 from romania_astar import romania_astar
 from read_graph_bfs import Grafo
+from paris_map_astar import *
 
 if __name__ == '__main__':
-    romania_astar('Arad', 'Bucharest')
+    print(distanciaEntreEstacoes('E1', 'E5'))
+    # romania_astar('Arad', 'Bucharest')
     # grafo = Grafo()
     # grafo.adicionaAresta(1, 2)
     # grafo.adicionaAresta(1, 3)
