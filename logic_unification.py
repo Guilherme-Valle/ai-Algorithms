2af441f9ffc5aa995d74e5ea0d5f48bf364de19b
Guilherme-Valle <guidovalle1608@gmail.com>
2021-06-08 23:56:18 -0300

    Inicia implementação unificação

---
+++ logic_unification.py
@@ -0,0 +1,33 @@
+import re
+
+loop = True
+base_de_conhecimento = {}
+while loop:
+    sentenca = input().rstrip()
+    if sentenca != 'END':
+        output_regex = re.search(r'(.*?)\((.*)\)', sentenca)
+        assinatura_funcao = output_regex.group(1)
+        if assinatura_funcao not in base_de_conhecimento:
+            base_de_conhecimento[assinatura_funcao] = []
+        parametros_funcao = output_regex.group(2).split(',')
+        base_de_conhecimento[assinatura_funcao].append(parametros_funcao)
+    else:
+        loop = False
+
+consulta = input().rstrip()
+output_regex = re.search(r'(.*?)\((.*)\)', consulta)
+
+assinatura_consulta = output_regex.group(1)
+parametros_consulta = output_regex.group(2).split(',')
+
+
+array_assinatura = base_de_conhecimento[assinatura_consulta]
+resposta = []
+print(base_de_conhecimento)
+for item in array_assinatura:
+    if item[0] == parametros_consulta[0]:
+        resposta.append(parametros_consulta[1] + '/' + item[1])
+    if item[1] == parametros_consulta[1]:
+        resposta.append(parametros_consulta[0] + '/' + item[0])
+
+print(', '.join(resposta))
