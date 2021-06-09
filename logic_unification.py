739d40d1f67370a141b03aaf97ccc56cac557bed
Guilherme-Valle <guidovalle1608@gmail.com>
2021-06-09 00:08:19 -0300

    ajusta unificaçao para os 3 primeiros casos

--- logic_unification.py
+++ logic_unification.py
@@ -23,11 +23,14 @@ parametros_consulta = output_regex.group(2).split(',')
 
 array_assinatura = base_de_conhecimento[assinatura_consulta]
 resposta = []
-print(base_de_conhecimento)
 for item in array_assinatura:
     if item[0] == parametros_consulta[0]:
         resposta.append(parametros_consulta[1] + '/' + item[1])
     if item[1] == parametros_consulta[1]:
         resposta.append(parametros_consulta[0] + '/' + item[0])
+    if parametros_consulta[0].isupper() and parametros_consulta[1].isupper():
+        resposta.append(parametros_consulta[0] + '/' + item[0])
+        resposta.append(parametros_consulta[1] + '/' + item[1])
 
-print(', '.join(resposta))
+
+print(','.join(resposta))
