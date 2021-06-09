11629544499b432c67c7ada451d3a3b44cf03d83
Guilherme-Valle <guidovalle1608@gmail.com>
2021-06-09 13:16:58 -0300

    adiciona inicio de questao backward chaining

---
+++ backward_chaining.py
@@ -0,0 +1,12 @@
+
+
+rules = int(input().rstrip())
+print(rules)
+for i in range(rules):
+    print(input().rstrip())
+
+base = input().rstrip()
+
+consulta = input().rstrip()
+print(base)
+print(consulta)
